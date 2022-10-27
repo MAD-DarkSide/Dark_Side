70b367d6207073a5d7bf71274b27b684c1b97ef2
IT21016684 <110347124+KanishkaGJ@users.noreply.github.com>
2022-10-27 23:55:44 +0530

    link testing

---
+++ app/src/main/java/com/example/madproject/login.java
@@ -0,0 +1,34 @@
+package com.example.madproject;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class login extends AppCompatActivity {
+
+     TextView textview;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login2);
+
+        textview = (TextView)findViewById(R.id.textView2);
+        textview.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(login.this,register.class);
+                startActivity(intent);
+
+//                Toast.makeText(login.this,"Transfer to register page",Toast.LENGTH_LONG).show();
+            }
+        });
+
+    }
+}
