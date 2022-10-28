8d7ef3a6debf7efd4d315e85d9d045610e8e29cf
Vinura de Silva <m.v.r.desilva@gmail.com>
2022-10-29 00:02:52 +0530

    Update login.java

--- app/src/main/java/com/example/madproject/login.java
+++ app/src/main/java/com/example/madproject/login.java
@@ -10,37 +10,39 @@ import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 
 public class login extends AppCompatActivity {
 
-    TextView textview;
-    private FirebaseAuth mAuth;
-
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login2);
-
-        mAuth = FirebaseAuth.getInstance();
-
-        textview = (TextView)findViewById(R.id.textView2);
-        textview.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(login.this,register.class);
-                startActivity(intent);
-
-                Toast.makeText(login.this,"Transfer to register page",Toast.LENGTH_LONG).show();
-            }
-        });
-
-        TextView userEmail = (TextView) findViewById(R.id.userEmail);
-        TextView password = (TextView) findViewById(R.id.password);
-
-        password = (EditText) findViewById(R.id.password);
-        userEmail = (EditText) findViewById(R.id.userEmail);
+//    TextView textview;
+//    private FirebaseAuth mAuth;
+//
+//
+//    @Override
+//    protected void onCreate(Bundle savedInstanceState) {
+//        super.onCreate(savedInstanceState);
+//        setContentView(R.layout.activity_login2);
+//
+//        mAuth = FirebaseAuth.getInstance();
+//
+//        textview = (TextView)findViewById(R.id.textView2);
+//        textview.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                Intent intent = new Intent(login.this,register.class);
+//                startActivity(intent);
+//
+//                Toast.makeText(login.this,"Transfer to register page",Toast.LENGTH_LONG).show();
+//            }
+//        });
+//
+//        TextView userEmail = (TextView) findViewById(R.id.userEmail);
+//        TextView password = (TextView) findViewById(R.id.password);
+//
+//        password = (EditText) findViewById(R.id.password);
+//        userEmail = (EditText) findViewById(R.id.userEmail);
 
 
 //        Button loginbtn = (Button) findViewById(R.id.loginButton);
@@ -50,7 +52,7 @@ public class login extends AppCompatActivity {
 //
 //        };
 
-    }
+//    }
 //    public void onClick(View v){
 //
 //        switch (v.getId()){
@@ -73,6 +75,44 @@ public class login extends AppCompatActivity {
 //        String userphone = editTextPhone.getText().toString().trim();
 //        String password = pass.getText().toString().trim();
 //    }
+ private EditText useremail;
+ private EditText password;
+ private Button login;
+ private FirebaseAuth mauth;
+
+ protected void onCreate(Bundle savedInstanceState) {
+
+     super.onCreate(savedInstanceState);
+     setContentView((R.layout.activity_login2));
+
+     useremail= findViewById(R.id.userEmail);
+     password = findViewById(R.id.password);
+     login = findViewById(R.id.loginButton);
+
+     mauth = FirebaseAuth.getInstance();
+
+     login.setOnClickListener(new View.OnClickListener() {
+         @Override
+         public void onClick(View view) {
+
+             String email = useremail.getText().toString();
+             String pass = password.getText().toString();
+             loginuser(email , pass);
+         }
+     });
+ }
+ private void loginuser(String email, String password){
+     mauth.signInWithEmailAndPassword(email, password).addOnSuccessListener(new OnSuccessListener<AuthResult>() {
+         @Override
+         public void onSuccess(AuthResult authResult) {
+             Toast.makeText(login.this,"LogIn Successful", Toast.LENGTH_SHORT).show();
+             startActivity(new Intent(login.this, MainActivity.class));
+             finish();
+
+         }
+     });
+ }
+
 }
 
 
