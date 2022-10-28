457f98bcd845f312ecd4ff2ce0089ad02dc700e5
Vinura de Silva <m.v.r.desilva@gmail.com>
2022-10-29 00:33:12 +0530

    Update login.java

--- app/src/main/java/com/example/madproject/login.java
+++ app/src/main/java/com/example/madproject/login.java
@@ -95,10 +95,7 @@ public class login extends AppCompatActivity {
          @Override
          public void onClick(View view) {
 
-             Intent intent = new Intent(login.this,register.class);
-              startActivity(intent);
-              
-              Toast.makeText(login.this,"Transfer to register page",Toast.LENGTH_LONG).show();
+
              String email = useremail.getText().toString();
              String pass = password.getText().toString();
              loginuser(email , pass);
