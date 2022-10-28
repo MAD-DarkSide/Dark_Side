a8644a750ca05a3c9aa1e5f4f0b67147b00f0b0d
Vinura de Silva <m.v.r.desilva@gmail.com>
2022-10-28 13:15:55 +0530

    Registration

--- app/src/main/java/com/example/madproject/register.java
+++ app/src/main/java/com/example/madproject/register.java
@@ -1,5 +1,6 @@
 package com.example.madproject;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
@@ -12,7 +13,11 @@ import android.widget.ProgressBar;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.FirebaseDatabase;
 
 import java.util.regex.Pattern;
 
@@ -115,6 +120,39 @@ public class register extends AppCompatActivity implements View.OnClickListener 
             editTextTextPassword.requestFocus();
             return;
         }
+        //progressBar.setVisibility(View.VISIBLE);
+
+        mAuth.createUserWithEmailAndPassword(useremail, password)
+                .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if(task.isSuccessful()){
+                            User user = new User(useremail, companyname, userphone);
+
+                            FirebaseDatabase.getInstance().getReference("Users")
+                                    .child(FirebaseAuth.getInstance().getCurrentUser().getUid())
+                                    .setValue(user).addOnCompleteListener(new OnCompleteListener<Void>() {
+                                        @Override
+                                        public void onComplete(@NonNull Task<Void> task) {
+                                            if (task.isSuccessful()){
+                                                Toast.makeText(register.this, "User has been Registered", Toast.LENGTH_LONG).show();
+                                                //progressBar.setVisibility(View.GONE);
+                                            }
+                                            else {
+                                                Toast.makeText(register.this, "Registration failed", Toast.LENGTH_LONG).show();
+                                                //progressBar.setVisibility(View.GONE);
+                                            }
+                                        }
+                                    });
+
+
+                        }
+                        else {
+                            Toast.makeText(register.this, "Registration failed", Toast.LENGTH_LONG).show();
+                            //progressBar.setVisibility(View.GONE);
+                        }
+                    }
+                });
 
     }
 }
