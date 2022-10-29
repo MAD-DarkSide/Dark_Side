902360e4ee5b770a94405e8cc690d81d1a70cb05
Vinura de Silva <m.v.r.desilva@gmail.com>
2022-10-30 01:28:23 +0530

    User profile created

--- app/src/main/java/com/example/madproject/userprofile.java
+++ app/src/main/java/com/example/madproject/userprofile.java
@@ -1,15 +1,29 @@
 package com.example.madproject;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class userprofile extends AppCompatActivity {
+
+    private FirebaseUser user;
+    private DatabaseReference reference;
+
+    private String userID;
 
     private Button logout;
 
@@ -27,5 +41,58 @@ public class userprofile extends AppCompatActivity {
                 startActivity(new Intent( userprofile.this,login.class));
             }
         });
+
+        user = FirebaseAuth.getInstance().getCurrentUser();
+        reference = FirebaseDatabase.getInstance().getReference("Users");
+
+        userID = user.getUid();
+
+        final TextView campanynametext = (TextView) findViewById(R.id.textView7);
+        final TextView emailtext = (TextView) findViewById(R.id.textView8);
+        final TextView phonetext = (TextView) findViewById(R.id.textView9);
+
+        reference.child(userID).addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                User userProfile = snapshot.getValue(User.class);
+
+                if(userProfile != null){
+                    String email = userProfile.userEmail;
+                    String companyname = userProfile.companyName;
+                    String phone = userProfile.userPhone;
+
+                    campanynametext.setText(companyname);
+                    emailtext.setText(email);
+                    phonetext.setText(phone);
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+                Toast.makeText(userprofile.this,"Something went wrong", Toast.LENGTH_LONG).show();
+
+            }
+        });
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
