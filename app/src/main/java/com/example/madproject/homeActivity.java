8080168290e3ca0072047cb847d944e9841c479a
IT21016684 <110347124+KanishkaGJ@users.noreply.github.com>
2022-10-30 10:43:09 +0530

    navigation update

--- app/src/main/java/com/example/madproject/homeActivity.java
+++ app/src/main/java/com/example/madproject/homeActivity.java
@@ -2,13 +2,32 @@ package com.example.madproject;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.Switch;
 
-public class homeActivity extends AppCompatActivity {
+public class homeActivity extends AppCompatActivity implements View.OnClickListener {
+    private ImageView option;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+        option = (ImageView)findViewById(R.id.option_icon);
+        option.setOnClickListener(this);
+
+    }
+
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.option_icon:
+                startActivity(new Intent(this, menu.class));
+                break;
+        }
     }
 }
+
+
